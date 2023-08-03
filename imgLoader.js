ea53a0b16e944d94dfd72eb5cebb93ea3b8c910e
MushroomFX <dergolem26@gmail.com>
2023-08-03 22:52:32 +0200

    more polished version

--- imgLoader.js
+++ imgLoader.js
@@ -33,5 +33,6 @@ const imgArr = [
 //not edit
 for(i =0;i<imgArr.length;i++){
     document.getElementById("items").innerHTML += `<img class="item" src="images/${imgArr[i]}"></img> `
+    if(i % 3 == 1)document.getElementsByClassName("item")[i].classList.add("odd")
     console.log(imgArr[i])
-  }
+}
